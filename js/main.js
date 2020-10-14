c2527494e59488ac08095b9527d0da829a20ec65
Tim Aadland <potetstapp@gmail.com>
2020-10-14 19:20:58 +0200

    Added a new countdown timer for christmas eve. When clicking on the black friday sale picture, it changes into a picture with christmas ornament lating on the floor with a new header 'Christmas Eve' and the countdownt time for it.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 // The  date we wanna countdown to
-const blackFriday = "27 nov 2020";
+const blackFridayDatee = "27 nov 2020";
+const christmasEveDatee = "24 dec 2020";
+
 
 // get the element from the webpage that has the specified id timer value. (Default 0 on the html file)
 const daysEl = document.getElementById("days");
@@ -7,35 +9,103 @@ const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("mins");
 const secondsEl = document.getElementById("secs");
 
+const header = document.querySelector("h1")
+
     // A function for finding how many days, hours, minutes and seconds it is from current time to black friday this year.
     // And display the countdown time till that day.
-    function countdownTimer(){
-    const blackFridayDate = new Date(blackFriday);
-    const currentDate = new Date();
-    // date time is shown in milliseconds, divide by 1000 to get the seconds
-    const totalSeconds = (blackFridayDate - currentDate) / 1000;
-    // 60 secs * 60 sec = 3600sec = 1h. 3600 sec * 24h = 86400 sec in a day(24h)
-    // Using Math.floor() for showing whole numbers
-    const days = Math.floor(totalSeconds / 86400);
-    // Use remainder of 24, as we want to know the remaining hours left of the day
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    // The same as hours, but here we want to get the minutes
-    const minutes = Math.floor(totalSeconds / 60) % 60;
-    // Same approach as minutes with getting the remaining seconds
-    const seconds = Math.floor(totalSeconds % 60)
+    function christmasEveCountdownTimer() {
+            const christmasEve = "Christmas Eve";
+            const christmasEveDate = new Date(christmasEveDatee);
+            const currentDate = new Date();
+            // date time is shown in milliseconds, divide by 1000 to get the seconds
+            const totalSeconds = (christmasEveDate - currentDate) / 1000;
+            // 60 secs * 60 sec = 3600sec = 1h. 3600 sec * 24h = 86400 sec in a day(24h)
+            // Using Math.floor() for showing whole numbers
+            const days = Math.floor(totalSeconds / 86400);
+            // Use remainder of 24, as we want to know the remaining hours left of the day
+            const hours = Math.floor(totalSeconds / 3600) % 24;
+            // The same as hours, but here we want to get the minutes
+            const minutes = Math.floor(totalSeconds / 60) % 60;
+            // Same approach as minutes with getting the remaining seconds
+            const seconds = Math.floor(totalSeconds % 60)
 
-    //Attach the time to the webpage
-    daysEl.innerHTML = days;
-    hoursEl.innerHTML = timeFormat(hours);
-    minutesEl.innerHTML = timeFormat(minutes);
-    secondsEl.innerHTML = timeFormat(seconds);
+            header.innerHTML = christmasEve;
+
+            //Attach the time to the webpage
+            daysEl.innerHTML = days;
+            hoursEl.innerHTML = timeFormat(hours);
+            minutesEl.innerHTML = timeFormat(minutes);
+            secondsEl.innerHTML = timeFormat(seconds);
+    }
+    // A function for finding how many days, hours, minutes and seconds it is from current time to black friday this year.
+    // And display the countdown time till that day.
+    function blackFridayCountdownTimer(){
+            const blackFriday = "Black Friday";
+            const blackFridayDate = new Date(blackFridayDatee);
+            const currentDate = new Date();
+            // date time is shown in milliseconds, divide by 1000 to get the seconds
+            const totalSeconds = (blackFridayDate - currentDate) / 1000;
+            // 60 secs * 60 sec = 3600sec = 1h. 3600 sec * 24h = 86400 sec in a day(24h)
+            // Using Math.floor() for showing whole numbers
+            const days = Math.floor(totalSeconds / 86400);
+            // Use remainder of 24, as we want to know the remaining hours left of the day
+            const hours = Math.floor(totalSeconds / 3600) % 24;
+            // The same as hours, but here we want to get the minutes
+            const minutes = Math.floor(totalSeconds / 60) % 60;
+            // Same approach as minutes with getting the remaining seconds
+            const seconds = Math.floor(totalSeconds % 60)
+
+            header.innerHTML = blackFriday;
+
+            //Attach the time to the webpage
+            daysEl.innerHTML = days;
+            hoursEl.innerHTML = timeFormat(hours);
+            minutesEl.innerHTML = timeFormat(minutes);
+            secondsEl.innerHTML = timeFormat(seconds);
 }
 // Display the 0 when the time gets below 10. e.g 09-01.
 function timeFormat(time) {
         return time < 10 ? `0${time}` : time;
 }
-
 // Setting an interval for the timer on the webpage. As it is counting in milliseconds, 1 sec = 1000ms
-setInterval(countdownTimer, 1000);
+
 // Initial call
-countdownTimer();
+/*
+function countdownChanger() {
+let bkImage = document.querySelector('img');
+
+bkImage.onclick = function () {
+    let imageSrc = bkImage.getAttribute("src");
+    if(imageSrc === "img/xmas.jpg") {
+        bkImage.setAttribute("src","img/sale.jpg");
+        blackFridayCountdownTimer();
+        let  handlebFriday = setInterval(blackFridayCountdownTimer, 1000);
+        let  handleXmass = setInterval(christmasEveCountdownTimer, 1000);
+        setTimeout(handleXmass)
+    } else {
+        bkImage.setAttribute("src","img/xmas.jpg");
+        christmasEveCountdownTimer();
+        let  handlebFriday = setInterval(blackFridayCountdownTimer, 1000);
+        let  handleXmass = setInterval(christmasEveCountdownTimer, 1000);
+        setTimeout(handlebFriday)
+    }
+}
+}
+*/
+var mysrc = "img/sale.jpg";
+function changeImage() {
+    let intervalStop = 0
+    if (mysrc == "img/sale.jpg") {
+        document.images["pic"].src = "img/sale.jpg";
+        document.images["pic"].alt = "Sale written on the window";
+        mysrc = "img/xmas.jpg";
+        blackFridayCountdownTimer();
+    } else {
+        document.images["pic"].src = "img/xmas.jpg";
+        document.images["pic"].alt = "Christmas ornament on floor";
+        mysrc = "img/sale.jpg";
+        christmasEveCountdownTimer();
+    }
+}
+
+changeImage();
